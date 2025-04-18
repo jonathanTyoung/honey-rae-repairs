0d0a38d921bd2acbe9b607bebd7876b8a9e1d9d3
Jon Young <youngt.jonathan>
2025-04-18 09:10:21 -0500

    updates

--- src/services/ticketService.jsx
+++ src/services/ticketService.jsx
@@ -3,8 +3,8 @@ export const getAllTickets = () => {
         res.json())
 }
 
-// export const getEmployeeById = (employeeId) => {
-//     return fetch(
-//         `http://localhost:8088/employees/${employeeId}?_expand=user`
-//     ).then((res) => res.json())
-// }
+export const getEmployeeById = (employeeId) => {
+    return fetch(
+        `http://localhost:8088/employees/${employeeId}?_expand=user`
+    ).then((res) => res.json())
+}
