e8a45c636d1a291fb36e62c3d4149261dc267ef7
Jon Young <youngt.jonathan>
2025-04-22 14:51:53 -0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import "./App.css"
 import { CustomerList } from "./components/customers/CustomerList.jsx"
 import { EmployeeList } from "./components/employees/EmployeeList.jsx"
+import { NavBar } from "./components/nav/NavBar.jsx"
 import { TicketList } from "./components/tickets/ticketList.jsx"
+import { Routes, Route, Outlet } from "react-router-dom"
 
 export const App = () => {
   return (
-  <>
-  {/* <TicketList/> */}
-  {/* <CustomerList /> */}
-  <EmployeeList />
-  </>
+    <Routes>
+      <Route path="/" element={
+        <>
+          <NavBar />
+          <Outlet />
+        </>
+      }
+      >
+        <Route path="tickets" element={<TicketList />} />
+        <Route path="customers" element={<CustomerList />} />
+        <Route path="employees" element={<EmployeeList />} />
+      </Route>
+    </Routes>
   )
 }
