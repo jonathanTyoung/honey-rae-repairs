d6c74f0de12dc01656c4d2b06243c9719cc7eb25
Jon Young <youngt.jonathan>
2025-04-24 14:06:42 -0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
 import "./App.css"
-import { CustomerList } from "./components/customers/CustomerList.jsx"
-import { EmployeeList } from "./components/employees/EmployeeList.jsx"
-import { NavBar } from "./components/nav/NavBar.jsx"
-import { TicketList } from "./components/tickets/ticketList.jsx"
 import { Routes, Route, Outlet } from "react-router-dom"
-import { Welcome } from "./components/welcome/Welcome.jsx"
-import { CustomerDetails } from "./components/customers/CustomerDetails.jsx"
-import { EmployeeDetails } from "./components/employees/EmployeeDetails.jsx"
 import { Login } from "./components/auth/Login"
 import { Register } from "./components/auth/Register"
 
@@ -15,27 +8,7 @@ export const App = () => {
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-
-      <Route path="/" element={
-        <>
-          <NavBar />
-          <Outlet />
-        </>
-      }
-      >
-        <Route index element={<Welcome />} />
-        <Route path="tickets" element={<TicketList />} />
-        <Route path="employees" element={<EmployeeList />} />
-        <Route path="employees">
-          <Route index element={<EmployeeList />} />
-          <Route path=":employeeId" element={<EmployeeDetails />} />
-        </Route>
-        <Route path="customers">
-          <Route index element={<CustomerList />} />
-          <Route path=":customerId" element={<CustomerDetails />} />
-        </Route>
-      </Route>
     </Routes>
   )
-  
+
 }
--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -1,3 +1,36 @@
+import { Route, Outlet, Routes } from "react-router-dom"
+import { NavBar } from "../components/nav/NavBar.jsx"
+import { Welcome } from "../components/welcome/Welcome.jsx"
+import { EmployeeDetails } from "../components/employees/EmployeeDetails.jsx"
+import { EmployeeList } from "../components/employees/EmployeeList.jsx"
+import { TicketList } from "../components/tickets/ticketList.jsx"
+import { CustomerList } from "../components/customers/CustomerList.jsx"
+import { CustomerDetails } from "../components/customers/CustomerDetails.jsx"
+
 export const ApplicationViews = () => {
-  return <></>
+  return (
+    <Routes>
+    <Route
+      path="/"
+      element={
+        <>
+          <NavBar />
+          <Outlet />
+        </>
+      }
+    >
+      <Route index element={<Welcome />} />
+      <Route path="tickets" element={<TicketList />} />
+      <Route path="employees" element={<EmployeeList />} />
+      <Route path="employees">
+        <Route index element={<EmployeeList />} />
+        <Route path=":employeeId" element={<EmployeeDetails />} />
+      </Route>
+      <Route path="customers">
+        <Route index element={<CustomerList />} />
+        <Route path=":customerId" element={<CustomerDetails />} />
+      </Route>
+    </Route>
+    </Routes>
+  )
 }
